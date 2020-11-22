34ae98451869280f164c8b5d54c612e8194e6af9
Andreas Ingeholm <andreas.ingeholm@gmail.com>
2020-11-22 10:50:03 +0100

    Pushed tutorial

---
+++ AdvancedSceneManagerExtras/Tutorial-Streaming/Scripts/GameStart.cs
@@ -0,0 +1,19 @@
+﻿using AdvancedSceneManager;
+using AdvancedSceneManager.Callbacks;
+using AdvancedSceneManager.Models;
+using AdvancedSceneManager.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStart : MonoBehaviour, ISceneOpen
+{
+    public UnityEvent _onGameStart;
+
+    public IEnumerator OnOpen()
+    {
+        _onGameStart.Invoke();
+        yield break;
+    }
+}
